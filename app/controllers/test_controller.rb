ee1f040530c728fb4a8570b295d0dbb3e469488c
Ricky Chilcott <ricky@rakefire.io>
2018-02-13 10:48:21 -0500

    Making it more configurable

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,8 +1,8 @@
 class TestController < ApplicationController
   def loop
-    job_checking_polling_time = 5 # seconds
-    max_poll_time_until_redirect = 20 #seconds
-    maximum_iterations = 30
+    job_checking_polling_time = params.fetch(:job_checking_polling_time, 5).to_f # seconds
+    max_poll_time_until_redirect = params.fetch(:max_poll_time_until_redirect, 20).to_i #seconds
+    maximum_iterations = params.fetch(:maximum_iterations, 30).to_i
     redirect_again = true
     iteration_start_time = Time.now
     params[:never_complete] ||= "false"
@@ -10,7 +10,7 @@ class TestController < ApplicationController
     job_identifier = build_or_fetch_job
     poll_iteration = params[:iteration].to_i + 1
 
-    if poll_iteration < maximum_iterations
+    if never_complete? || poll_iteration <= maximum_iterations
       Rails.logger.info("We're into another poll iteration")
 
       while (Time.now - iteration_start_time) < max_poll_time_until_redirect
@@ -24,7 +24,12 @@ class TestController < ApplicationController
         end
       end
 
-      redirect_to(action: :loop, iteration: poll_iteration, job_id: job_identifier, never_complete: params[:never_complete]) if redirect_again
+      redirect_to(action: :loop, iteration: poll_iteration,
+                                 job_id: job_identifier,
+                                 job_checking_polling_time: job_checking_polling_time,
+                                 max_poll_time_until_redirect: max_poll_time_until_redirect,
+                                 maximum_iterations: maximum_iterations,
+                                 never_complete: never_complete?) if redirect_again
     else
       render json: {status: :failure, job_id: job_identifier, message: "Got to iteration #{poll_iteration}. Too many iterations"}
     end
@@ -40,9 +45,13 @@ class TestController < ApplicationController
       SecureRandom.hex(32)
     end
 
+    def never_complete?
+      params[:never_complete] == "true"
+    end
+
     # This would be checking in Sidekiq whether the job is still queued or in progress
     def job_done?(identifier)
-      v = if params[:never_complete] == "true"
+      v = if never_complete?
             0
           else
             rand(100)
